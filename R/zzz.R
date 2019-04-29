dcc365c9498c417143395a5f686cf6bb8b02b20e
Michael Treadwell <mtreadwell@interworks.com>
2019-04-29 10:07:03 -0500

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   message <- paste("This version of alterryx was built and tested for",
-                   "Alteryx Server 2018.4. Support for earlier versions is",
+                   "Alteryx Server 2019.1. Support for earlier versions is",
                    "not guaranteed")
   packageStartupMessage(message)
 }
